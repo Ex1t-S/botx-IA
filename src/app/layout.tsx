814b300470a7c2caf0fdcabe9f16ad7a84b33882
Ex1t-S <tuemail@gmail.com>
2026-03-18 14:50:00 -0300

    Favicon Ready xD

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,10 +6,15 @@ export const metadata: Metadata = {
 	title: "BOTX",
 	description: "Search dead wallets cryptocurrency, best in the market.",
 	icons: {
-    icon: "/images/favicon.png",
-    shortcut: "/images/favicon.png",
-    apple: "/images/favicon.png",
-  },
+	icon: [
+		{ url: "/images/favicon.ico" },
+		{ url: "/images/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+		{ url: "/images/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+	],
+	shortcut: "/images/favicon.ico",
+	apple: "/images/apple-touch-icon.png",
+},
+manifest: "/images/site.webmanifest",
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
